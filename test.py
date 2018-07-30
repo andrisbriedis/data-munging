146e47b5f5dedd9d09398884ed6950bc16412ba8
Andris Briedis <andris@printify.com>
2018-07-30 10:41:09 +0300

    PART 2 DONE

--- test.py
+++ test.py
@@ -12,6 +12,14 @@ class Measurement(object):
     def delta(self):
         return self.max - self.min
 
+class Score(object):
+    def __init__(self, name, forward, against):
+        self.name = name
+        self.forward = forward
+        self.against = against
+
+    def delta(self):
+        return abs(self.forward - self.against)
 
 class MainTest(TestCase):
     def smoke_test(self):
@@ -22,38 +30,78 @@ class MainTest(TestCase):
 
         weather = Weather(lines)
 
-    def load_test_data(self):
-        data = open("weather.dat")
+    def load_test_data(self,filename):
         lines = []
-        for line in data:
-            lines.append(line)
+        with open(filename) as data:
+            for line in data:
+                lines.append(line)
+
         return lines
 
+    def test_football_file_all_lines_loaded(self):
+        lines = self.load_test_data('football.dat')
+        self.assertEqual(len(lines), 22)
+
     def test_file_all_lines_loaded(self):
-        lines = self.load_test_data()
-        self.assertEquals(len(lines), 33)
+        lines = self.load_test_data('weather.dat')
+        self.assertEqual(len(lines), 33)
 
     def test_can_normalize_data(self):
-        lines = self.load_test_data()
+        lines = self.load_test_data('weather.dat')
         normal = self.normalize(lines)
         first_measurement = normal[0]
         last_measurement = normal[-1]
-        self.assertEquals(1, first_measurement.day)
-        self.assertEquals(59, first_measurement.min)
-        self.assertEquals(88, first_measurement.max)
-        self.assertEquals(29, first_measurement.delta())
+        self.assertEqual(1, first_measurement.day)
+        self.assertEqual(59, first_measurement.min)
+        self.assertEqual(88, first_measurement.max)
+        self.assertEqual(29, first_measurement.delta())
 
-        self.assertEquals(30, last_measurement.day)
-        self.assertEquals(45, last_measurement.min)
-        self.assertEquals(90, last_measurement.max)
-        self.assertEquals(45, last_measurement.delta())
+        self.assertEqual(30, last_measurement.day)
+        self.assertEqual(45, last_measurement.min)
+        self.assertEqual(90, last_measurement.max)
+        self.assertEqual(45, last_measurement.delta())
 
     def test_find_lowest_delta_day(self):
-        lines = self.load_test_data()
+        lines = self.load_test_data('weather.dat')
         normal = self.normalize(lines)
 
         result = sorted(normal, key=lambda measurement: measurement.delta())
-        self.assertEquals(14, result[0].day)
+        self.assertEqual(14, result[0].day)
+
+
+    def test_can_normalize_football_data(self):
+        lines = self.load_test_data('football.dat')
+        normal = self.normalize_football(lines)
+        first_measurement = normal[0]
+        last_measurement = normal[-1]
+        self.assertEqual('Arsenal', first_measurement.name)
+        self.assertEqual(79, first_measurement.forward)
+        self.assertEqual(36, first_measurement.against)
+        self.assertEqual(43, first_measurement.delta())
+
+        self.assertEqual('Leicester', last_measurement.name)
+        self.assertEqual(30, last_measurement.forward)
+        self.assertEqual(64, last_measurement.against)
+        self.assertEqual(34, last_measurement.delta())
+
+    def test_find_lowest_delta_team(self):
+        lines = self.load_test_data('football.dat')
+        normal = self.normalize_football(lines)
+
+        result = sorted(normal, key=lambda score: score.delta())
+        self.assertEqual("Aston_Villa", result[0].name)
+
+    def normalize_football(self, data):
+        team_score = []
+        for row in data[1:]:
+            if row.strip().startswith('-----'):
+                continue
+            line = row.split()
+            name = line[1]
+            forward = int(line[6])
+            against = int(line[8])
+            team_score.append(Score(name, forward, against))
+        return team_score
 
     def normalize(self, data):
         measurements = []
